83fcb713d8028640252085b086161203d7d9dfaf
Sergey Prokopenko <svprokopenko92@gmail.com>
2020-03-19 14:58:34 +0700

    Revert "выпилил контекст"
    This reverts commit 0ef25d117e67e8c81ad6f0a7fd1b7389eb96ee9b.

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,94 +1,100 @@
-import React, { useCallback, useMemo, useState, useEffect } from 'react';
+import React, { useCallback, useContext, useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Paper, Grid, styled } from '@material-ui/core';
 import css from './style.module.css';
 import logo from '../../../asstets/logo.png';
+import {Context} from '../../../Context/context';
 import LoginContent from './LoginContent';
 import SignupContent from '../signup/SignupContent';
 //container
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { auth, login as actionLogin } from '../../../dugs/user';
 
 
 const FullContainer = styled(Paper)({
-  height: '98vh',
-  padding: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+    height: '98vh',
+    padding: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
 });
 
 const LoginLayout = () => {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorPassword, setErrorPassword] = useState('');
-  const isLoggin = useSelector(store => !!store.user.isLoggin);
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorPassword, setErrorPassword] = useState('')
+    const isLoggin = useSelector(store => !!store.user.isLoggin)
 
-  useEffect(() => {
-    if (isLoggin) {
-      history.replace('/map');
-    }
-  }, [isLoggin]);
+    useEffect(()=> {
+        if (isLoggin) {
+            history.replace('/map');
+        }
+    }, [isLoggin])
 
-  const [active] = useState(false);
-  const getBgStyle = useMemo(() => {
-    return css.main__bg;
-  }, []);
-  const preventDefault = event => event.preventDefault();
-  const history = useHistory();
+    const [active] = useState(false)
+    const {login} = useContext(Context);
+    const getBgStyle = useMemo(() => {
+        return css.main__bg;
+    }, [])
+    const preventDefault = event => event.preventDefault();
+    const history = useHistory();
 
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
-    if (password.length && email.length) {
-      dispatch(actionLogin({ email: email, password }));
-      return;
-    }
-    setErrorPassword('Одно из полей пустое!');
-  }, [email, password]);
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+        const result = login({email: email, password});
+        dispatch(actionLogin({email: email, password}));
+        if (!result) {
+            history.replace('/map')
+        } else {
+            setErrorPassword(result.error)
+        }
 
-  const handleEmailChange = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
+    }, [email, password])
 
-  const handlePasswordChange = useCallback(event => {
-    setPassword(event.target.value);
-  }, []);
+    const handleEmailChange = useCallback((e) => {
+        setEmail(e.target.value);
+    }, [])
 
-  return (
-    <FullContainer className={getBgStyle} data-testid="login-layout">
-      <Grid item xs={3}>
-        <img src={logo} alt=""/>
-      </Grid>
-      <Grid item xs={3}>
-        {!active ? <LoginContent
-          email={email}
-          password={password}
-          errorPassword={errorPassword}
-          getBgStyle={getBgStyle}
-          preventDefault={preventDefault}
-          handleSubmit={handleSubmit}
-          handleEmailChange={handleEmailChange}
-          handlePasswordChange={handlePasswordChange}
-        /> : <SignupContent/>}
-      </Grid>
-    </FullContainer>
-  );
-};
+    const handlePasswordChange = useCallback(event => {
+        setPassword(event.target.value);
+    }, [])
+    
+    return (
+        <FullContainer className={getBgStyle} data-testid="login-layout">
+             <Grid item xs={3}>
+                <img src={logo} alt=""/>
+             </Grid>
+             <Grid item xs={3}>
+                 {!active ? <LoginContent
+                     email={email}
+                     password={password}
+                     errorPassword={errorPassword}
+                     login={login}
+                     getBgStyle={getBgStyle}
+                     preventDefault={preventDefault}
+                     handleSubmit={handleSubmit}
+                     handleEmailChange={handleEmailChange}
+                     handlePasswordChange={handlePasswordChange}
+                 /> : <SignupContent/>}
+             </Grid>
+         </FullContainer>
+    );
+}
 
 LoginLayout.prototype = {
-  email: PropTypes.string.isRequired,
-  changeForm: PropTypes.func,
-  password: PropTypes.string.isRequired,
-  errorPassword: PropTypes.object,
-  login: PropTypes.func,
-  getBgStyle: PropTypes.func,
-  preventDefault: PropTypes.func,
-  handleSubmit: PropTypes.func,
-  handleEmailChange: PropTypes.func,
-  handlePasswordChange: PropTypes.func,
-};
+    email: PropTypes.string.isRequired,
+    changeForm: PropTypes.func,
+    password: PropTypes.string.isRequired,
+    errorPassword: PropTypes.object,
+    login: PropTypes.func,
+    getBgStyle: PropTypes.func,
+    preventDefault: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    handleEmailChange: PropTypes.func,
+    handlePasswordChange: PropTypes.func
+}
 
 export default LoginLayout;
