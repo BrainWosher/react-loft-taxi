6a79f31acfe4d9769efeca081362ce88d6fe675c
Sergey Prokopenko <svprokopenko92@gmail.com>
2020-04-09 14:39:12 +0700

    добавил код сплиттинг Lazy Suspense

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Redirect, Switch } from 'react-router-dom';
@@ -6,20 +6,14 @@ import { connect } from 'react-redux';
 
 import './../App.css';
 import Header from './header/Header';
-import Login from './pages/login/Login';
-import Map from './pages/map/Map';
-import Profile from './pages/profile/Profile';
-import Signup from './../components/pages/signup/Signup';
 import PrivateRoute from './../components/common/PrivateRoute';
 import routes from '../routes/index';
 import { logout } from '../dugs/user';
 
-const getComponents = {
-  profile: Profile,
-  map: Map,
-  login: Login,
-  signup: Signup,
-};
+const Login = lazy(()=> import('./pages/login/Login'));
+const Map = lazy(()=> import('./pages/map/Map'));
+const Profile = lazy(()=> import('./pages/profile/Profile'));
+const Signup = lazy(()=> import('./pages/signup/Signup'));
 
 class Main extends Component {
   static propTypes = {
@@ -51,12 +45,14 @@ class Main extends Component {
             isLoggedIn={this.props.isAuth}
             logout={this.props.logout}
           />
-          <Switch>
-            <PrivateRoute path={'/profile'} component={getComponents['profile']}/>
-            <PrivateRoute path={'/map'} component={getComponents['map']} />
-            <Route path={'/login'} component={getComponents['login']} />
-            <Route path={'/signup'} component={getComponents['signup']} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <PrivateRoute path={'/profile'} component={Profile}/>
+              <PrivateRoute path={'/map'} component={Map} />
+              <Route path={'/login'} component={Login} />
+              <Route path={'/signup'} component={Signup} />
+            </Switch>
+          </Suspense>
           <Redirect to={this.props.isAuth ? 'map' : 'login'} />
         </BrowserRouter>
       </div>
